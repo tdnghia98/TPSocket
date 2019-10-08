552b29107137a925e7c5cc23cf67edd421a6d82f
duy.nghia.tran <duy.nghia.tran@ibm.com>
2019-10-08 22:28:10 +0200

    multicast

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -2,11 +2,8 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
+import java.net.*;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class ClientGUI extends JFrame implements ActionListener, KeyListener, WindowListener {
     private JButton sendBtn;
@@ -18,23 +15,23 @@ public class ClientGUI extends JFrame implements ActionListener, KeyListener, Wi
     private int id;
     private Client client;
 
-    public ClientGUI() {
+    public ClientGUI() throws UnknownHostException {
         sendBtn = new JButton("Send");
         sendBtn.setBounds(410, 600, 80, 50);
         sendBtn.addActionListener(this);
 
         startBtn = new JButton("Join");
-        startBtn.setBounds(this.getWidth()/2, this.getSize().height/2, 100, 50);
+        startBtn.setBounds(this.getWidth() / 2, this.getSize().height / 2, 100, 50);
         startBtn.addActionListener(this);
 
         conversationTextArea = new JTextArea();
-        conversationTextArea.setBounds(10,10,480, 550);
+        conversationTextArea.setBounds(10, 10, 480, 550);
 
         messageInputTextField = new JTextField();
         messageInputTextField.setBounds(10, 600, 390, 50);
         messageInputTextField.addKeyListener(this);
         idLabel = new JLabel();
-        idLabel.setBounds(10,575,100,10);
+        idLabel.setBounds(10, 575, 100, 10);
 
         add(startBtn);
         setSize(500, 700);
@@ -49,7 +46,11 @@ public class ClientGUI extends JFrame implements ActionListener, KeyListener, Wi
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == sendBtn) {
-            client.sendMessage();
+            try {
+                client.sendMessage();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         } else if (e.getSource() == startBtn) {
             try {
                 client.start();
@@ -65,8 +66,12 @@ public class ClientGUI extends JFrame implements ActionListener, KeyListener, Wi
     }
 
 
-    public static void main (String[] args) {
-        ClientGUI cGui = new ClientGUI();
+    public static void main(String[] args) {
+        try {
+            ClientGUI cGui = new ClientGUI();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -78,7 +83,11 @@ public class ClientGUI extends JFrame implements ActionListener, KeyListener, Wi
     public void keyPressed(KeyEvent e) {
         if (e.getSource() == messageInputTextField) {
             if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                client.sendMessage();
+                try {
+                    client.sendMessage();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
@@ -88,7 +97,9 @@ public class ClientGUI extends JFrame implements ActionListener, KeyListener, Wi
 
     }
 
-    public int getId() {return id;}
+    public int getId() {
+        return id;
+    }
 
     @Override
     public void windowOpened(WindowEvent e) {
@@ -97,7 +108,11 @@ public class ClientGUI extends JFrame implements ActionListener, KeyListener, Wi
 
     @Override
     public void windowClosing(WindowEvent e) {
-        client.sendMessage("/quit");
+        try {
+            client.sendMessage("/quit");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
@@ -128,57 +143,47 @@ public class ClientGUI extends JFrame implements ActionListener, KeyListener, Wi
     class Client {
         private String server;
         private int port;
-        Socket socket = null;
-        private BufferedReader socketInput = null;
-        BufferedReader stdIn = null;
-        private PrintStream socketOutput = null;
+        InetAddress serverAddress;
+        MulticastSocket socket = null;
         ArrayList<String> messages;
 
-        public Client() {
-            server = "localhost";
-            port = 5100;
+        public Client() throws UnknownHostException {
+            server = "239.0.0.0";
+            port = 1234;
+            serverAddress = InetAddress.getByName(server);
             messages = new ArrayList<String>();
         }
 
-        public boolean start() throws IOException {
+        void start() throws IOException {
+            byte[] buf = new byte[256];
+
             try {
-                socket = new Socket(server, port);
+                socket = new MulticastSocket(port);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
+            socket.joinGroup(serverAddress);
             conversationTextArea.setText("[DEBUG] Connected. Connection : " + socket.getInetAddress() + ":" + socket.getPort());
 
-            try {
-                socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                socketOutput = new PrintStream(socket.getOutputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                id = Integer.parseInt(socketInput.readLine());
-                idLabel.setText("Your id: " + id);
-                add(idLabel);
-                System.out.println("Id received: " + id);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            //id = Integer.parseInt();
+            idLabel.setText("Your id: " + id);
+            add(idLabel);
+            System.out.println("Id received: " + id);
             new ListenFromServer().start();
-            return true;
         }
 
-        private void sendMessage() {
+        private void sendMessage() throws IOException {
             String message = messageInputTextField.getText();
             sendMessage(message);
         }
 
-        private void sendMessage(String msg) {
+        private void sendMessage(String msg) throws IOException {
             if (!msg.isEmpty()) {
-
-                    this.socketOutput.println(msg);
-                    messageInputTextField.setText("");
-                    messageInputTextField.requestFocus();
+                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(), msg.getBytes().length, serverAddress, port);
+                socket.send(msgPacket);
+                messageInputTextField.setText("");
+                messageInputTextField.requestFocus();
 
                 if (msg.equals("/quit")) {
                     System.exit(0);
@@ -191,19 +196,22 @@ public class ClientGUI extends JFrame implements ActionListener, KeyListener, Wi
         class ListenFromServer extends Thread {
             public void run() {
                 while (true) {
-                    String message = null;
+                    byte[] buf = new byte[256];
+                    DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
                     try {
-                        message = socketInput.readLine();
+                        socket.receive(msgPacket);
+                        String message = new String(buf, 0, buf.length);
                         messages.add(message);
                         conversationTextArea.append("\n" + message);
+                        System.out.println(message);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    System.out.println(message);
-                }
+
             }
         }
-
     }
+
+}
 
 }
